70b4bd26b2e0b6a0d9917db96ecdd24278212ebf
Volcomix <volcomix@github.com>
2015-06-07 21:00:22 +0200

    Add MITM proxy

--- src/Proxy.ts
+++ src/Proxy.ts
@@ -4,6 +4,8 @@
 
 import express = require('express');
 import http = require('http');
+import https = require('https');
+import fs = require('fs');
 import net = require('net');
 import httpProxy = require('http-proxy');
 import url = require('url');
@@ -17,19 +19,37 @@ proxy.on('error', function(err, req, res) {
 var app = express();
 
 app.use(function(req, res) {
+	var reqUrl: url.Url;
+	if (req.secure) {
+		reqUrl = url.parse(url.resolve(req.protocol + '://' + req.header('host'), req.url));
+	} else {
+		reqUrl = url.parse(req.url);
+	}
+	
 	var options: httpProxy.Options = {
-		target: req.protocol + '://' + url.parse(req.url).host
+		target: reqUrl.protocol + '//' + reqUrl.host
 	};
-	console.log(options.target);
+	
+	console.log(reqUrl.href);
+	
 	proxy.web(req, res, options);
 });
 
-var server = http.createServer(app);
+var proxyServer = http.createServer(app);
 
-server.on('connect', function(req: http.IncomingMessage, cltSocket: net.Socket, head: { [key: string]: string; }) {
-	var srvUrl = url.parse('https://' + req.url);
-	console.log(srvUrl.href);
-	var srvSocket = net.connect(parseInt(srvUrl.port), srvUrl.hostname, function() {
+var options = {
+	key: fs.readFileSync('keys/key.pem'),
+	cert: fs.readFileSync('keys/cert.pem')
+};
+var mitmServer = https.createServer(options, app).listen(3129, function() {
+	var host = mitmServer.address().address;
+	var port = mitmServer.address().port;
+
+	console.log('Internal MITM server listening at https://%s:%s', host, port);
+});
+
+proxyServer.on('connect', function(req: http.IncomingMessage, cltSocket: net.Socket, head: { [key: string]: string; }) {
+	var srvSocket = net.connect(mitmServer.address().port, mitmServer.address().address, function() {
 		cltSocket.write(
 			'HTTP/1.1 200 Connection Established\r\n' +
 			'\r\n');
@@ -43,10 +63,10 @@ server.on('connect', function(req: http.IncomingMessage, cltSocket: net.Socket, 
 	});
 });
 
-server.listen(3128, function() {
+proxyServer.listen(3128, function() {
 
-	var host = server.address().address;
-	var port = server.address().port;
+	var host = proxyServer.address().address;
+	var port = proxyServer.address().port;
 
 	console.log('Proxy listening at http://%s:%s', host, port);
 
