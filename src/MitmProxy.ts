08801d0a457f9f92f6774febf11620d72d83ae61
Volcomix <volcomix@gmail.com>
2015-06-30 21:31:28 +0200

    Add MitmProxy class

---
+++ src/MitmProxy.ts
@@ -0,0 +1,58 @@
+/// <reference path="../typings/node/node.d.ts"/>
+/// <reference path="../typings/express/express.d.ts"/>
+/// <reference path="../typings/request/request.d.ts"/>
+
+import url = require('url');
+
+import express = require('express');
+import request = require('request');
+
+import CA = require('./CertificateAuthority');
+import MitmServer = require('./MitmServer');
+import ProxyServer = require('./ProxyServer');
+
+class MitmProxy {
+
+	constructor(private app = express(), private ca?: CA, private verbose?: boolean,
+		private proxyVerbose?: boolean, private mitmVerbose?: boolean) {
+
+		app.use((req, res) => {
+			var reqUrl: string;
+			if (req.secure) {
+				reqUrl = url.resolve(req.protocol + '://' + req.header('host'), req.url);
+			} else {
+				reqUrl = req.url;
+			}
+
+			if (this.verbose) {
+				console.log(reqUrl);
+			}
+
+			req.pipe(request(reqUrl)).pipe(res);
+		});
+	}
+
+	listen(proxyPort = 3128, mitmPort = 3129) {
+
+		var mitmServer = new MitmServer(this.app, this.ca, this.mitmVerbose)
+			.listen(mitmPort, () => {
+				var host = mitmServer.address.address;
+				var port = mitmServer.address.port;
+
+				if (this.verbose) {
+					console.log('MITM server listening at https://%s:%s', host, port);
+				}
+			});
+
+		var proxyServer = new ProxyServer(this.app, mitmServer, this.proxyVerbose)
+			.listen(proxyPort, () => {
+				var host = proxyServer.address.address;
+				var port = proxyServer.address.port;
+
+				console.log('Proxy listening at http://%s:%s', host, port);
+
+			});
+	}
+}
+
+export = MitmProxy;
--- src/Proxy.ts
+++ src/Proxy.ts
@@ -1,45 +1,5 @@
 /// <reference path="../typings/node/node.d.ts"/>
-/// <reference path="../typings/express/express.d.ts"/>
-/// <reference path="../typings/request/request.d.ts"/>
 
-import url = require('url');
+import MitmProxy = require('./MitmProxy');
 
-import express = require('express');
-import request = require('request');
-
-import CA = require('./CertificateAuthority');
-import MitmServer = require('./MitmServer');
-import ProxyServer = require('./ProxyServer');
-
-var app = express();
-
-app.use(function(req, res) {
-    var reqUrl: string;
-    if (req.secure) {
-        reqUrl = url.resolve(req.protocol + '://' + req.header('host'), req.url);
-    } else {
-        reqUrl = req.url;
-    }
-
-    console.log(reqUrl);
-
-    req.pipe(request(reqUrl)).pipe(res);
-});
-
-var ca = new CA('FR', 'Some-State', 'Freemedia', 'Freemedia');
-
-var mitmServer = new MitmServer(app, ca).listen(3129, () => {
-    var host = mitmServer.address.address;
-    var port = mitmServer.address.port;
-
-    console.log('MITM server listening at https://%s:%s', host, port);
-});
-
-var proxyServer = new ProxyServer(app, mitmServer).listen(3128, () => {
-
-    var host = proxyServer.address.address;
-    var port = proxyServer.address.port;
-
-    console.log('Proxy listening at http://%s:%s', host, port);
-
-});
+new MitmProxy().listen();
