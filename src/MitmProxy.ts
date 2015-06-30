e0415ec96d8ce39d72e2cc6cb0ee319437464435
Volcomix <volcomix@gmail.com>
2015-07-01 00:17:31 +0200

    Add callback for MitmProxy startup

--- src/MitmProxy.ts
+++ src/MitmProxy.ts
@@ -1,11 +1,13 @@
 /// <reference path="../typings/node/node.d.ts"/>
 /// <reference path="../typings/express/express.d.ts"/>
 /// <reference path="../typings/request/request.d.ts"/>
+/// <reference path="../typings/q/Q.d.ts"/>
 
 import url = require('url');
 
 import express = require('express');
 import request = require('request');
+import Q = require('q');
 
 import CA = require('./CertificateAuthority');
 import MitmServer = require('./MitmServer');
@@ -13,8 +15,11 @@ import ProxyServer = require('./ProxyServer');
 
 class MitmProxy {
 
-	constructor(private app = express(), private ca?: CA, private verbose?: boolean,
-		private proxyVerbose?: boolean, private mitmVerbose?: boolean) {
+	private mitmServer: MitmServer;
+	private proxyServer: ProxyServer;
+
+	constructor(app = express(), ca?: CA,
+		private verbose?: boolean, proxyVerbose?: boolean, mitmVerbose?: boolean) {
 
 		app.use((req, res) => {
 			var reqUrl: string;
@@ -30,28 +35,36 @@ class MitmProxy {
 
 			req.pipe(request({ uri: reqUrl, followRedirect: false })).pipe(res);
 		});
+
+		this.mitmServer = new MitmServer(app, ca, mitmVerbose);
+		this.proxyServer = new ProxyServer(app, this.mitmServer, proxyVerbose)
 	}
 
-	listen(proxyPort = 3128, mitmPort = 3129) {
+	listen(proxyPort = 3128, mitmPort = 3129, listeningListener?: () => void) {
+		Q.all([
+			Q.Promise((resolve) => {
+				this.mitmServer.listen(mitmPort, () => {
+					var host = this.mitmServer.address.address;
+					var port = this.mitmServer.address.port;
 
-		var mitmServer = new MitmServer(this.app, this.ca, this.mitmVerbose)
-			.listen(mitmPort, () => {
-				var host = mitmServer.address.address;
-				var port = mitmServer.address.port;
+					if (this.verbose) {
+						console.log('MITM server listening at https://%s:%s', host, port);
+					}
 
-				if (this.verbose) {
-					console.log('MITM server listening at https://%s:%s', host, port);
-				}
-			});
+					resolve({});
+				});
+			}),
+			Q.Promise((resolve) => {
+				this.proxyServer.listen(proxyPort, () => {
+					var host = this.proxyServer.address.address;
+					var port = this.proxyServer.address.port;
 
-		var proxyServer = new ProxyServer(this.app, mitmServer, this.proxyVerbose)
-			.listen(proxyPort, () => {
-				var host = proxyServer.address.address;
-				var port = proxyServer.address.port;
+					console.log('Proxy listening at http://%s:%s', host, port);
 
-				console.log('Proxy listening at http://%s:%s', host, port);
-
-			});
+					resolve({});
+				});
+			})
+		]).done(() => { listeningListener(); });
 	}
 }
 
--- test/MitmProxy.ts
+++ test/MitmProxy.ts
@@ -14,14 +14,22 @@ import MitmProxy = require('../src/MitmProxy');
 describe('MitmProxy', function() {
 	var ca: CA;
 	var mitmProxy: MitmProxy;
-	before(function() {
-		ca = new CA('FR', 'Some-State', 'TestMitmProxy', 'TestMitmProxy');
-		mitmProxy = new MitmProxy(undefined, ca);
+
+	describe('#listen()', function() {
+		before(function() {
+			ca = new CA('FR', 'Some-State', 'TestMitmProxy', 'TestMitmProxy');
+		});
+		it('should start', function(done) {
+			new MitmProxy(undefined, ca).listen(13132, 13133, done);
+		});
+		it('should proxy HTTP requests (http://example.com)');
+		it('should proxy HTTPS requests (https://example.com)');
 	});
-	it('should start');
-	it('should proxy HTTP requests (http://example.com)');
-	it('should proxy HTTPS requests (https://example.com)');
-	it('should stop');
+	describe('#close()', function() {
+		it('should stop');
+		it('should not proxy HTTP requests anymore (http://example.com)');
+		it('should not proxy HTTPS requests anymore (https://example.com)');
+	});
 	after(function() {
 		return Q.nfcall(fs.unlink, 'keys/TestMitmProxy-key.pem').finally(function() {
 			return Q.nfcall(fs.unlink, 'keys/TestMitmProxy-CA-cert.pem');
